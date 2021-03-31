53724747bbabbea68a3a992f9e873d164bb76276
unknown <mickykumar510@gmail.com>
2021-03-31 12:02:51 -0400

    update
    make resume site

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for site project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 
